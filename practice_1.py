8df7eb43e186aacc0e1ba177d57e1fe3ab446b42
soopertramp <pradeepchandrareddy95@gmail.com>
2023-02-03 11:50:28 +0530

    math module

--- practice_1.py
+++ practice_1.py
@@ -23,4 +23,33 @@ a = 4
 
 c = (x ** 2) / y + (z/a)
 
+# Answer these 3 questions without typing code. Then type code to check your answer.
+
+# What is the value of the expression 4 * (6 + 5) = 44
+
+# What is the value of the expression 4 * 6 + 5 = 29
+
+# What is the value of the expression 4 + 6 * 5 = 34
+
+4 * (6 + 5)
+
+4 * 6 + 5
+
+4 + 6 * 5
+
+# What is the type of the result of the expression 3 + 1.5 + 4? = float
+
+a = 3 + 1.5 + 4
+type(a)
+
+# What would you use to find a number’s square root, as well as its square?
+
+10 ** 2 #square
+
+import math
+
+number = 121
+
+square_root = math.sqrt(number)
+print(square_root)
 
