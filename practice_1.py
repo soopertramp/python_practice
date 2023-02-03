48968f0ce6445d3e0cf1bc9c686d2d99c2c461a7
soopertramp <pradeepchandrareddy95@gmail.com>
2023-02-03 11:34:22 +0530

    Initial Commit

--- practice_1.py
+++ practice_1.py
@@ -1,6 +1,18 @@
-variable = "pradeep"
+# Test your knowledge. 
+# Answer the following Questions
 
-def square(x):
-    return x**2
+# Numbers: 1, 5, 20
 
-square(5)
+# Strings: "Pradeep"
+
+# Lists: [1, 2, "pradeep", 3.4]
+
+# Tuples: (1, 2, "pradeep", 3.4)
+
+# Dictionaries: {"a" : "pradeep", "b" : "chandra", "c" : 10}
+
+# Numbers
+#Write an equation that uses multiplication, division, an exponent, addition, and subtraction that is equal to 100.25.
+
+#Hint: This is just to test your memory of the basic arithmetic commands, work backwards from 100.25
+
