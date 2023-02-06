8a3603cb5dd0ecbee38d46f1769644b62b59272a
soopertramp <pradeepchandrareddy95@gmail.com>
2023-02-06 11:36:09 +0530

    Complete

--- practice_2.py
+++ practice_2.py
@@ -20,3 +20,47 @@ for i in range(1,51):
         
 a = [i for i in range(1,51) if i % 3 == 0]
 a
+
+# Go through the string below and if the length of a word is even print "even!"
+
+st = 'Print every word in this sentence that has an even number of letters'
+
+for word in st.split():
+    if len(word) % 2 == 0:
+        print(word)
+        
+""" Write a program that prints the integers from 1 to 100. But for multiples of three print "Fizz" 
+instead of the number, and for the multiples of five print "Buzz". For numbers which are multiples of 
+both three and five print "FizzBuzz"."""
+
+lst = list(range(1,101))
+lst
+
+for number in lst:
+    if number % 3 == 0:
+        print("Fizz")
+    if number % 5 == 0:
+        print("Buzz")
+    if number % 3 == 0 and number % 5 == 0:
+        print("FizzBuzz")
+    else:
+        print(number)
+        
+for number in lst:
+    if number % 3 == 0:
+        print("Fizz")
+    elif number % 5 == 0:
+        print("Buzz")
+    elif number % 3 == 0 and number % 5 == 0:
+        print("FizzBuzz")
+    else:
+        print(number)
+
+# Use List Comprehension to create a list of the first letters of every word in the string below:
+
+st = 'Create a list of the first letters of every word in this string'
+
+for word in st.split():
+    print(word[0])
+    
+[word[0] for word in st.split()]
