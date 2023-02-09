6c24aa7cd441e90d6ea5ad07d152229fc920565e
soopertramp <pradeepchandrareddy95@gmail.com>
2023-02-09 19:25:18 +0530

    completed

--- practice_2.py
+++ practice_2.py
@@ -63,4 +63,4 @@ st = 'Create a list of the first letters of every word in this string'
 for word in st.split():
     print(word[0])
     
-[word[0] for word in st.split()]
+[word[0] for word in st.split()] 
